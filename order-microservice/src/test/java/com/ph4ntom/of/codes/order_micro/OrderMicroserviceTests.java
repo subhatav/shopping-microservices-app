ae1fe2d9d28371a8104d40269f02f121253718c1
Subhatav Dhali <subhatav@gmail.com>
2023-05-27 16:25:44 +0530

    Create a basic Maven project for the Order Microservice

---
+++ order-microservice/src/test/java/com/ph4ntom/of/codes/order_micro/OrderMicroserviceTests.java
@@ -0,0 +1,11 @@
+package com.ph4ntom.of.codes.order_micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMicroserviceTests {
+
+  @Test
+  void contextLoads() {}
+}
