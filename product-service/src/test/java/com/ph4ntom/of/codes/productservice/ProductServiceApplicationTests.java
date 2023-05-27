70147f84bb15f650ce4baa2c935b46178a1830ea
Subhatav Dhali <subhatav@gmail.com>
2023-05-27 12:56:49 +0530

    Create a basic Maven project for the Product Service module

---
+++ product-service/src/test/java/com/ph4ntom/of/codes/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ph4ntom.of.codes.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
