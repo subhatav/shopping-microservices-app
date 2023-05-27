9d12ad20efbefcedce16a26d5ba68e47909f251d
Subhatav Dhali <subhatav@gmail.com>
2023-05-27 20:23:26 +0530

    Create a basic Maven project for the Inventory Microservice

---
+++ inventory-microservice/src/main/java/com/ph4ntom/of/codes/inventory_micro/InventoryMicroservice.java
@@ -0,0 +1,13 @@
+package com.ph4ntom.of.codes.inventory_micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMicroservice {
+
+  public static void main(final String[] arguments) {
+
+    SpringApplication.run(InventoryMicroservice.class, arguments);
+  }
+}
